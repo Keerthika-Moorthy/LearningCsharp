7dec43cd7154ae6a4ba0ee501a689faa4d0e006d
Keerthika-Moorthy <68242704+Keerthika-Moorthy@users.noreply.github.com>
2020-07-23 12:52:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SingletonClass sc = new SingletonClass();
+           sc.Message("Request Message from Manager");
+
+            SingletonClass scobj  = new SingletonClass();
+            scobj.Message("Request Message from Employee");
+
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ SingletonClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    class SingletonClass
+    {
+        static int instanceCounter = 0;
+        public SingletonClass()
+        {
+            instanceCounter++;
+            Console.WriteLine("Instances created " + instanceCounter);
+        } 
+        public void Message(string message)
+        {
+           Console.WriteLine("Message " + message);
+        }
+    }
+}
